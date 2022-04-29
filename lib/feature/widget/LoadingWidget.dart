08c89f40aa53b24a1e77fcaf0c72aefdf9a94920
Ahmet GEZMEN <ahmet_gezmen_@hotmail.com>
2022-04-29 19:12:46 +0300

    loading widget butona basilarak çağırılması sağlandı

--- lib/feature/widget/LoadingWidget.dart
+++ lib/feature/widget/LoadingWidget.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
 
+void LoadingWidgetButton(BuildContext context) {
+  final alertDialog = AlertDialog(
+    title: LoadingWidget(),
+  );
+
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return alertDialog;
+    },
+  );
+}
+
+
+
 class LoadingWidget extends StatelessWidget {
   const LoadingWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+
+      // Loading widget burada container kismina eklenecek
+
+    );
   }
 }
