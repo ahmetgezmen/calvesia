f10698e52aeeaae81c00bd933d20f15f2800e256
Ahmet GEZMEN <ahmet_gezmen_@hotmail.com>
2022-04-30 19:24:14 +0300

    Yaklaşan etkinlik cardları boyutlandırıldı ve contaıner eklendı

--- lib/feature/widget/UpcomingEventsCardWidget.dart
+++ lib/feature/widget/UpcomingEventsCardWidget.dart
@@ -5,27 +5,40 @@ class UpcomingEventsCardWidget extends StatefulWidget {
   UpcomingEventsCardWidget({Key? key, required this.title}) : super(key: key);
 
   @override
-  State<UpcomingEventsCardWidget> createState() => _UpcomingEventsCardWidgetState();
+  State<UpcomingEventsCardWidget> createState() =>
+      _UpcomingEventsCardWidgetState();
 }
 
 class _UpcomingEventsCardWidgetState extends State<UpcomingEventsCardWidget> {
-
- @override
+  @override
   void didUpdateWidget(covariant UpcomingEventsCardWidget oldWidget) {
-   super.didUpdateWidget(oldWidget);
-   if (oldWidget.title != widget.title) {
-     setState(() {});
-   }
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.title != widget.title) {
+      setState(() {});
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return  Padding(
+    return Padding(
       padding: const EdgeInsets.only(left: 40, right: 40, top: 20),
       child: Card(
-        elevation: 5,
-        child: Center(
-          child: Text(widget.title),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15.0),
+        ),        elevation: 5,
+        child: Row(
+          children: <Widget>[
+            SizedBox(
+              height: 100,
+              width: 100,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Color(0xffd966a5),
+                  borderRadius: BorderRadius.circular(15.0)
+                ) ,
+              ),
+            )
+          ],
         ),
       ),
     );
