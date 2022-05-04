9e09bd0a77332d00d3f56b59cda486b9bde2746d
Ahmet GEZMEN <ahmet_gezmen_@hotmail.com>
2022-05-04 13:33:21 +0300

    Riverpod kutuphanesi kuruldu ve providerscaope my app uzerine eklendi

--- lib/core/base/initializedFireBase.dart
+++ lib/core/base/initializedFireBase.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'AppBase.dart';
 import 'firebase_options.dart';
 import 'package:flutter/material.dart';
@@ -11,6 +12,6 @@ Future<void> geIinitializedMain() async {
   );
   FirebaseDatabase.instance.databaseURL= "https://calvesia-default-rtdb.europe-west1.firebasedatabase.app";
   runApp(
-    MyApp(),
+    ProviderScope(child: MyApp()),
   );
 }
