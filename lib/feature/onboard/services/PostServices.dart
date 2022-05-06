6bc4c36429abb564db71a7c8df32cc2acc7d3d81
Ahmet GEZMEN <ahmet_gezmen_@hotmail.com>
2022-05-06 20:57:52 +0300

    getPostServices Fonksiyonu yazildi

--- lib/feature/onboard/services/PostServices.dart
+++ lib/feature/onboard/services/PostServices.dart
@@ -3,32 +3,29 @@ import 'dart:convert';
 import 'package:calvesia/feature/onboard/models/PostModel.dart';
 import 'package:firebase_database/firebase_database.dart';
 
-
-
 DatabaseReference ref = FirebaseDatabase.instance.ref("posts");
 
 class PostRepo {
-  
-    static Future<bool> addPostServices(post) async {
-      try{
-        await ref.push().set(jsonDecode(postModelToJson(post)));
-        return true;
-      }on Error {
-        return false;
-      }
+  static Future<bool> addPostServices(post) async {
+    try {
+      await ref.push().set(jsonDecode(postModelToJson(post)));
+      return true;
+    } on Error {
+      return false;
     }
+  }
 
 
-//
-//   getPost(){
-//     DatabaseReference starCountRef =
-//     FirebaseDatabase.instance.ref('posts/$postId/');
-//     starCountRef.onValue.listen((DatabaseEvent event) {
-//       final data = event.snapshot.value;
-//       updateStarCount(data);
-//     });
-//   }
-//
+
+  static getPostServidces(postId) async {
+    late PostModel postModelData;
+    DataSnapshot snapshot = await FirebaseDatabase.instance.ref('posts/$postId/').get();
+    if (snapshot.exists) {
+      return postModelFromJson(jsonEncode(snapshot.value));
+    } else {
+      return null;
+    }}
+
 //   getPostList(){
 //
 //   }
