34759e6de4b05e40b4c398b17517dcabb0ff9b1a
Ahmet GEZMEN <ahmet_gezmen_@hotmail.com>
2022-05-20 19:46:04 +0300

    explorePage.dart dosyasi clean code formatina cekildi

--- lib/feature/pages/view/ExplorePage.dart
+++ lib/feature/pages/view/ExplorePage.dart
@@ -1,7 +1,4 @@
-import 'dart:convert';
-
 import 'package:calvesia/Utils/Style/ColorPalette.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
@@ -10,7 +7,7 @@ import '../../widget/PopularEventCardWidget.dart';
 import '../models/post_model.dart';
 
 class ExplorePage extends StatefulWidget {
-  static const List Category = [
+  static const List category = [
     {"name": "Parti", "color": BaseColorPalet.PartyColor, "tag": 'party'},
     {"name": "Kariyer", "color": BaseColorPalet.CareerColor, "tag": 'career'},
     {"name": "Sağlık", "color": BaseColorPalet.HealthColor, "tag": 'health'},
@@ -28,7 +25,7 @@ class ExplorePage extends StatefulWidget {
 
 class _ExplorePageState extends State<ExplorePage> {
   Future<void> _refeshIndicator() {
-    return Future.delayed(Duration(seconds: 0));
+    return Future.delayed(const Duration(seconds: 0));
   }
 
   @override
@@ -39,8 +36,8 @@ class _ExplorePageState extends State<ExplorePage> {
       child: Column(
         children: [
           Padding(
-            padding: const EdgeInsets.only(left: 10.0,right: 10.0),
-            child: Container(
+            padding: const EdgeInsets.only(left: 10.0, right: 10.0),
+            child: SizedBox(
               height: 50,
               child: ListView.separated(
                 scrollDirection: Axis.horizontal,
@@ -49,20 +46,20 @@ class _ExplorePageState extends State<ExplorePage> {
                   return Padding(
                     padding: const EdgeInsets.all(4.0),
                     child: CategoryButton(
-                      title: ExplorePage.Category[index]["name"],
-                      buttonColor: ExplorePage.Category[index]["color"],
+                      title: ExplorePage.category[index]["name"],
+                      buttonColor: ExplorePage.category[index]["color"],
                     ),
                   );
                 },
                 separatorBuilder: (BuildContext context, int index) {
-                  return SizedBox(
+                  return const SizedBox(
                     width: 30,
                   );
                 },
               ),
             ),
           ),
-          SilverDelegateComponent(),
+          const SilverDelegateComponent(),
         ],
       ),
     );
@@ -118,6 +115,7 @@ class _SilverDelegateComponentState extends State<SilverDelegateComponent> {
     }
   }
 
+  @override
   void dispose() {
     _pagingController.dispose();
     super.dispose();
@@ -148,6 +146,7 @@ class _SilverDelegateComponentState extends State<SilverDelegateComponent> {
     );
   }
 }
+
 class CategoryButton extends StatefulWidget {
   final Color buttonColor;
   final String title;
@@ -186,7 +185,6 @@ class _CategoryButtonState extends State<CategoryButton> {
         onPressed: () {
           setState(() {
             _isAktive == true ? _isAktive = false : _isAktive = true;
-            print(_isAktive);
           });
         },
         child: Text(widget.title),
@@ -206,18 +204,15 @@ class CardForDashBoard extends StatelessWidget {
       shadowColor: Colors.orangeAccent,
       elevation: 10.0,
       child: Column(
-        children: [
+        children: const [
           Expanded(
             child: Placeholder(),
           ),
           Expanded(
-            child: Container(
-              child: Text("Title"),
-            ),
+            child: Text("Title"),
           ),
         ],
       ),
     );
   }
 }
-
