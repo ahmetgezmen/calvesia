818d8ecf1389d93cd5940e9e3c922c2f381c419e
merve guhan <merve.guhan@gmail.com>
2022-05-22 15:43:39 +0300

    email_alredy_use_widget.dart düzenlendi

--- lib/feature/authencitation/signup/view/widget/email_alredy_use_widget.dart
+++ lib/feature/authencitation/signup/view/widget/email_alredy_use_widget.dart
@@ -1,17 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class EmailAlredyUseWidget extends StatelessWidget {
   const EmailAlredyUseWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final Widget svg = SvgPicture.asset("assets/images/Component 15.svg",
+        semanticsLabel: 'Bu e-posta zaten kullanılıyor logosu'
+    );
+    return Container(
+        child:Column(
+            children:[
+              svg,
+              Text('Bu E-posta Zaten Kullanılıyor',
+                  style: TextStyle(
+                      fontFamily:'PTSans' ,
+                      fontSize: 18,
+                      color: Colors.black38
+                  )
+              )
+            ]
+        )
+    );
   }
 }
 
 void EmailAlredyUseWidgetFunction(BuildContext context) {
   final alertDialog = AlertDialog(
     title: EmailAlredyUseWidget(),
+    shape: RoundedRectangleBorder(
+        borderRadius:
+        BorderRadius.circular(20)
+    ),
   );
 
   showDialog(
