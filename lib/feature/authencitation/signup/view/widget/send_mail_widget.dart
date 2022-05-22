b5f7d9ec1e0056ad18c518a7808efa1645a04f52
merve guhan <merve.guhan@gmail.com>
2022-05-22 15:45:05 +0300

    send_mail_widget.dart düzenlendi

--- lib/feature/authencitation/signup/view/widget/send_mail_widget.dart
+++ lib/feature/authencitation/signup/view/widget/send_mail_widget.dart
@@ -1,17 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class SendEmailWidget extends StatelessWidget {
   const SendEmailWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final Widget svg = SvgPicture.asset("assets/images/Component 7.svg",
+        semanticsLabel: 'Sana bir -e posta gönderdik logosu'
+    );
+
+    return Container(
+
+        child:Column(
+            children:[
+              svg,
+              Text('Sana Bir E-posta Gönderdik',
+                  style: TextStyle(
+                      fontFamily:'PTSans' ,
+                      fontSize: 18,
+                      color: Colors.black38
+                  )
+              ),
+              Text('E-posta/daki kodu girerek hesabını onaylayabilirsin',
+                  style: TextStyle(
+                      fontFamily:'PTSans' ,
+                      fontSize: 12,
+                      color: Colors.black12
+                  )
+              )
+            ]
+        )
+    );
   }
 }
 
 void SendEmailWidgetFunction(BuildContext context) {
   final alertDialog = AlertDialog(
     title: SendEmailWidget(),
+    shape: RoundedRectangleBorder(
+        borderRadius:
+        BorderRadius.circular(20)
+    ),
   );
 
   showDialog(
