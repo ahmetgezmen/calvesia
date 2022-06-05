55d13df737df211a7cdd7c455d9fe333125a869e
Ahmet GEZMEN <ahmet_gezmen_@hotmail.com>
2022-06-05 23:36:18 +0300

    onboard sayfasinda bottomsheet duzeltmesi

--- lib/feature/onboard/on_board_page.dart
+++ lib/feature/onboard/on_board_page.dart
@@ -10,13 +10,11 @@ import 'widgets/page_indicator.dart';
 class OnboardingPage extends StatefulWidget {
   const OnboardingPage({Key? key}) : super(key: key);
 
-
   @override
   State<OnboardingPage> createState() => _OnboardingPageState();
 }
 
 class _OnboardingPageState extends State<OnboardingPage> {
-
   final controller = PageController();
   bool isLastPage = false;
 
@@ -28,7 +26,6 @@ class _OnboardingPageState extends State<OnboardingPage> {
   }
 
   Widget buildPage({
-
     required String urlImage,
     required String title,
     required String subtitle,
@@ -75,166 +72,179 @@ class _OnboardingPageState extends State<OnboardingPage> {
     final double screenWidth = MediaQuery.of(context).size.width;
     final double screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
-      body: PageView(
-        controller: controller,
-        onPageChanged: (index) {
-          setState(() => isLastPage = index == 3);
-        },
+      body: Stack(
         children: [
-          buildPage(
-              urlImage: "assets/images/ob_page_1.png",
-              title: "Etrafındaki Etkinlikleri Keşfet!",
-              subtitle:
-                  "Üniversitelere özel etkinliklerden anında haberdar olabilir, \n İstersen sen de etkinlik oluşturabilirsin."),
-          buildPage(
-              urlImage: "assets/images/ob_page_2.png",
-              title: "Etrafındaki Etkinlikleri Keşfet!",
-              subtitle:
-                  "Üniversitelere özel etkinliklerden anında haberdar olabilir, \n İstersen sen de etkinlik oluşturabilirsin."),
-          buildPage(
-              urlImage: "assets/images/ob_page_3.png",
-              title: "Etrafındaki Etkinlikleri Keşfet!",
-              subtitle:
-                  "Üniversitelere özel etkinliklerden anında haberdar olabilir, \n İstersen sen de etkinlik oluşturabilirsin."),
-          Container(
-            constraints: const BoxConstraints.expand(),
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                  image: Image.asset("assets/images/ob_page_4.png").image,
-                  fit: BoxFit.cover),
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                 SizedBox(height: screenHeight / 7.1),
-                const Text(
-                  "Yunivent",
-                  style: TextStyle(
-                    color: Color(0xfff6f9ff),
-                    fontSize: 32,
-                    fontWeight: FontWeight.bold,
-                  ),
+          PageView(
+            controller: controller,
+            onPageChanged: (index) {
+              setState(() => isLastPage = index == 3);
+            },
+            children: [
+              buildPage(
+                  urlImage: "assets/images/ob_page_1.png",
+                  title: "Etrafındaki Etkinlikleri Keşfet!",
+                  subtitle:
+                      "Üniversitelere özel etkinliklerden anında haberdar olabilir, \n İstersen sen de etkinlik oluşturabilirsin."),
+              buildPage(
+                  urlImage: "assets/images/ob_page_2.png",
+                  title: "Etrafındaki Etkinlikleri Keşfet!",
+                  subtitle:
+                      "Üniversitelere özel etkinliklerden anında haberdar olabilir, \n İstersen sen de etkinlik oluşturabilirsin."),
+              buildPage(
+                  urlImage: "assets/images/ob_page_3.png",
+                  title: "Etrafındaki Etkinlikleri Keşfet!",
+                  subtitle:
+                      "Üniversitelere özel etkinliklerden anında haberdar olabilir, \n İstersen sen de etkinlik oluşturabilirsin."),
+              Container(
+                constraints: const BoxConstraints.expand(),
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: Image.asset("assets/images/ob_page_4.png").image,
+                      fit: BoxFit.cover),
                 ),
-                 SizedBox(height: screenHeight / 32.54),
-                Container(
-                  padding: const EdgeInsets.symmetric(),
-                  child: const Text(
-                    "Hoşgeldiniz",
-                    style: TextStyle(color: Color(0xfff6f9ff)),
-                  ),
-                ),
-                 SizedBox(height: screenHeight / 7.1),
-                SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.55,
-                  height: MediaQuery.of(context).size.width * 0.14,
-                  child: Expanded(
-                    child: ElevatedButton(
-                      style: ButtonStyle(
-                          elevation: MaterialStateProperty.all(0),
-                          backgroundColor: MaterialStateProperty.all(const Color(0Xffffb85a)),
-                          foregroundColor: MaterialStateProperty.all(const Color(0xfff6f9ff))
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(height: screenHeight / 7.1),
+                    const Text(
+                      "Yunivent",
+                      style: TextStyle(
+                        color: Color(0xfff6f9ff),
+                        fontSize: 32,
+                        fontWeight: FontWeight.bold,
                       ),
-                      child:  const Text("GİRİŞ YAP"),
-                      onPressed: () {
-                        Navigator.of(context).push(
-                            MaterialPageRoute(builder:  (context) {
-                              return const LoginPageScreen();
-                            },)
-                        );
-                      },
                     ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 16.0),
-                  child: SizedBox(
-                    width: MediaQuery.of(context).size.width * 0.55,
-                    height: MediaQuery.of(context).size.width * 0.14,
-                    child: Expanded(
-                      child: ElevatedButton(
-                        style: ButtonStyle(
-                            elevation: MaterialStateProperty.all(0),
-                            backgroundColor: MaterialStateProperty.all(const Color(0xffc04757)),
-                            foregroundColor: MaterialStateProperty.all(const Color(0xfff6f9ff))
+                    SizedBox(height: screenHeight / 32.54),
+                    Container(
+                      padding: const EdgeInsets.symmetric(),
+                      child: const Text(
+                        "Hoşgeldiniz",
+                        style: TextStyle(color: Color(0xfff6f9ff)),
+                      ),
+                    ),
+                    SizedBox(height: screenHeight / 7.1),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.55,
+                      height: MediaQuery.of(context).size.width * 0.14,
+                      child: Expanded(
+                        child: ElevatedButton(
+                          style: ButtonStyle(
+                              elevation: MaterialStateProperty.all(0),
+                              backgroundColor: MaterialStateProperty.all(
+                                  const Color(0Xffffb85a)),
+                              foregroundColor: MaterialStateProperty.all(
+                                  const Color(0xfff6f9ff))),
+                          child: const Text("GİRİŞ YAP"),
+                          onPressed: () {
+                            Navigator.of(context).push(MaterialPageRoute(
+                              builder: (context) {
+                                return const LoginPageScreen();
+                              },
+                            ));
+                          },
                         ),
-                        child:  const Text("KAYIT OL",),
-                        onPressed: () {
-                          Navigator.of(context).push(
-                              MaterialPageRoute(builder:  (context) {
-                                return const SingUpPAgeScreen();
-                              },)
-                          );
-
-                        },
                       ),
                     ),
-                  ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 16.0),
+                      child: SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.55,
+                        height: MediaQuery.of(context).size.width * 0.14,
+                        child: Expanded(
+                          child: ElevatedButton(
+                            style: ButtonStyle(
+                                elevation: MaterialStateProperty.all(0),
+                                backgroundColor: MaterialStateProperty.all(
+                                    const Color(0xffc04757)),
+                                foregroundColor: MaterialStateProperty.all(
+                                    const Color(0xfff6f9ff))),
+                            child: const Text(
+                              "KAYIT OL",
+                            ),
+                            onPressed: () {
+                              Navigator.of(context).push(MaterialPageRoute(
+                                builder: (context) {
+                                  return const SingUpPAgeScreen();
+                                },
+                              ));
+                            },
+                          ),
+                        ),
+                      ),
+                    ),
+                    TextButton(
+                      style: TextButton.styleFrom(
+                        primary: const Color(0xfff6f9ff),
+                        textStyle: const TextStyle(
+                          fontSize: 14,
+                          decoration: TextDecoration.underline,
+                        ),
+                      ),
+                      onPressed: () async {
+                        try {
+                          await FirebaseAuth.instance.signInAnonymously();
+                          Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) => const BasePageMiddleWave(),
+                          ));
+                        } on FirebaseAuthException catch (_) {
+                          somethingWrongWidgetFunction(context);
+                        }
+                      },
+                      child: const Text(
+                        'Uygulamaya Göz At',
+                      ),
+                    ),
+                  ],
                 ),
-                TextButton(
-                  style: TextButton.styleFrom(
-                    primary: const Color(0xfff6f9ff),
-                    textStyle: const TextStyle(
-                      fontSize: 14,
-                      decoration: TextDecoration.underline,
+              ),
+            ],
+          ),
+          Expanded(
+              child: Align(
+            child: isLastPage
+                ? Container(
+                    height: 0,
+                  )
+                : Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 15),
+                    height: screenHeight / 9.76,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        TextButton(
+                          child: const Text(
+                            "Atla",
+                            style: TextStyle(
+                              color: Color(0xfff6f9ff),
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          onPressed: () => controller.jumpToPage(3),
+                        ),
+                        Center(
+                          child: PageIndicator(controller: controller),
+                        ),
+                        TextButton(
+                          child: const Text(
+                            "İleri",
+                            style: TextStyle(
+                              color: Color(0xfff6f9ff),
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          onPressed: () => controller.nextPage(
+                              duration: const Duration(milliseconds: 500),
+                              curve: Curves.easeInOut),
+                        ),
+                      ],
                     ),
                   ),
-                  onPressed: () async {
-                    try {
-                      await FirebaseAuth.instance.signInAnonymously();
-                      Navigator.of(context).push(MaterialPageRoute(builder: (context) => const BasePageMiddleWave(),));
-                    } on FirebaseAuthException catch (_) {
-                      somethingWrongWidgetFunction(context);
-                    }
-                  },
-                  child: const Text(
-                    'Uygulamaya Göz At',
-                  ),
-                ),
-              ],
-            ),
-          ),
+            alignment: Alignment.bottomCenter,
+          ))
         ],
       ),
-      bottomSheet: isLastPage
-          ? Container(
-              height: 0,
-            )
-          : Container(
-              padding: const EdgeInsets.symmetric(horizontal: 15),
-              height: screenHeight / 9.76,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  TextButton(
-                    child: const Text(
-                      "Atla",
-                      style: TextStyle(
-                        color: Color(0xfff6f9ff),
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    onPressed: () => controller.jumpToPage(3),
-                  ),
-                  Center(
-                    child: PageIndicator(controller: controller),
-                  ),
-                  TextButton(
-                    child: const Text(
-                      "İleri",
-                      style: TextStyle(
-                        color: Color(0xfff6f9ff),
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    onPressed: () => controller.nextPage(
-                        duration: const Duration(milliseconds: 500),
-                        curve: Curves.easeInOut),
-                  ),
-                ],
-              ),
-            ),
     );
   }
 }
