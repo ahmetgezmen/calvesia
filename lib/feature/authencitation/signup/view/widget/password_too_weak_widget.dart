8f0c2b8574133249b82c40f7eeae5ffd9cca3ab1
merve guhan <merve.guhan@gmail.com>
2022-06-06 13:29:52 +0300

    password_too_weak_widget.dart düzenlendi

--- lib/feature/authencitation/signup/view/widget/password_too_weak_widget.dart
+++ lib/feature/authencitation/signup/view/widget/password_too_weak_widget.dart
@@ -1,23 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class PasswordTooWeakWidget extends StatelessWidget {
   const PasswordTooWeakWidget({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Container();
+  Widget build(BuildContext context) {final Widget svg = SvgPicture.asset("assets/images/Component 23.svg",
+      semanticsLabel: 'Şifre çok zayıf logosu'
+  );
+
+  return Column(
+      children:[
+        svg,
+        const Text('Şifre çok zayıf, lütfen şifrenize farklı türde karakterler ekleyiniz ',
+            style: TextStyle(
+                fontFamily:'PTSans' ,
+                fontSize: 18,
+                color: Colors.black38
+            )
+        )
+      ]
+  );
   }
 }
 
-void passwordTooWeakWidgetFunction(BuildContext context) {
-  const alertDialog =  AlertDialog(
-    title:  PasswordTooWeakWidget(),
+
+
+Future<bool> passwordTooWeakWidgetFunction (BuildContext context)async {
+  final alertDialog = AlertDialog(
+    title: const PasswordTooWeakWidget(),
+    shape: RoundedRectangleBorder(
+        borderRadius:
+        BorderRadius.circular(20)
+    ),
   );
 
-  showDialog(
-    context: context,
-    builder: (BuildContext context) {
-      return alertDialog;
-    },
+  await showDialog(
+      context: context,
+      builder: (BuildContext context) {
+    return alertDialog;
+  },
   );
+  return true;
 }
